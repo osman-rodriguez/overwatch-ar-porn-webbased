65d5901da858dbfbba4a9c852d86bec8076c39a5
osman-rodriguez <107283250+osman-rodriguez@users.noreply.github.com>
2022-06-11 17:02:12 -0500

    Update
    -Added a preloader
    -Small bugs fixed

---
+++ README.md
@@ -0,0 +1,2 @@
+# overwatch-ar-porn-webbased
+Porn web app to watch the female heroes of overwatch (nudes) in augmented reality, web-based application.
---
+++ assets/scripts/preloader.js
@@ -0,0 +1,10 @@
+ $(window).load(function() {
+   $('.preloader').fadeOut('slow');
+});
+document.addEventListener('DOMContentLoaded', function() {
+    var scene = document.querySelector('a-scene');
+    var splash = document.querySelector('#splash');
+    scene.addEventListener('loaded', function (e) {
+        splash.style.display = 'none';
+    });
+});
